852bf55b404f8ec4615803f911b5fcbc677ca663
Savitar465 <55471147+Savitar465@users.noreply.github.com>
2021-03-22 20:34:38 -0400

    swagger integration

--- src/main/java/com/bo/xMarket/XMarketApplication.java
+++ src/main/java/com/bo/xMarket/XMarketApplication.java
@@ -2,8 +2,10 @@ package com.bo.xMarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class XMarketApplication {
 
 	public static void main(String[] args) {
