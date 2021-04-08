d3e7a5752e51313c6a2a4b9e3a3132c3b32c2b2e
Fulano2141 <macuto2142dt@gmail.com>
2021-04-08 18:44:21 -0400

    Admin login and findbypersonId

---
+++ src/main/java/com/bo/xMarket/api/AdminApi.java
@@ -0,0 +1,32 @@
+package com.bo.xMarket.api;
+
+import com.bo.xMarket.bl.AdminBl;
+import com.bo.xMarket.dto.AdminRequest;
+import com.bo.xMarket.dto.LoginRequest;
+import com.bo.xMarket.dto.ManagerRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/admin")
+public class AdminApi {
+    private AdminBl adminBl;
+
+    @Autowired
+    public AdminApi(AdminBl adminBl) {
+        this.adminBl = adminBl;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    public AdminRequest userSignUp(@RequestBody LoginRequest LoginRequest) {
+        return adminBl.adminLogin(LoginRequest);
+    }
+
+    @RequestMapping(value = "/login/{personId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public AdminRequest getInfoManager(@PathVariable("personId") Integer id) {
+        return adminBl.infoManager(id);
+    }
+}
---
+++ src/main/java/com/bo/xMarket/dto/AdminRequest.java
@@ -0,0 +1,126 @@
+package com.bo.xMarket.dto;
+
+public class AdminRequest {
+    private Integer personId;
+    private String name;
+    private String surname;
+    private String username;
+    private String email;
+    private String userPhoto;
+    private String description;
+    private String cellphone;
+    private String gender;
+    private String password;
+
+    public AdminRequest(Integer personId, String name, String surname, String username, String email, String userPhoto, String description, String cellphone, String gender, String password) {
+        this.personId = personId;
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.email = email;
+        this.userPhoto = userPhoto;
+        this.description = description;
+        this.cellphone = cellphone;
+        this.gender = gender;
+        this.password = password;
+    }
+
+    public AdminRequest() {
+    }
+
+    @Override
+    public String toString() {
+        return "AdminRequest{" +
+                "personId=" + personId +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", userPhoto='" + userPhoto + '\'' +
+                ", description='" + description + '\'' +
+                ", cellphone='" + cellphone + '\'' +
+                ", gender='" + gender + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Integer personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserPhoto() {
+        return userPhoto;
+    }
+
+    public void setUserPhoto(String userPhoto) {
+        this.userPhoto = userPhoto;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
